ceb2b7971d05e18208fab0f79f33bc22f40651b5
rhoulihan <rick.houlihan@gmail.com>
2022-01-17 20:35:59 -0600

    Update MongoReader.java

--- src/main/java/MongoReader.java
+++ src/main/java/MongoReader.java
@@ -1,6 +1,7 @@
 package com.mongodb.CollectionLoader;
 
 import org.bson.Document;
+import java.util.regex.Pattern;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
@@ -9,6 +10,8 @@ public class MongoReader implements Runnable {
 	private String orderId;
 	private MongoCollection<Document> coll;
 
+	/* Simulates N devices inserting X Documents */
+
 	public MongoReader(MongoCollection<Document> coll, String orderId) {
 		this.coll = coll;
 		this.orderId = orderId;
@@ -16,7 +19,7 @@ public class MongoReader implements Runnable {
 
 	public void run() {
 		MongoCursor<Document> cursor = null;
-		Document query = new Document("_id", new Document("$gte", orderId));
+		Document query = new Document("_id", new Document("$regex", Pattern.compile(String.format("^%s",orderId))));
 		
 		try {
 			cursor = coll.find(query).iterator();
